0db4fdc23946378b09956d0e699885957d92fed4
Nagesh Kumar <85521267+nkchauhan003@users.noreply.github.com>
2026-05-12 21:33:27 +0530

    Container With Most Water

---
+++ src/main/java/com/backendml/array/B4_ContainerWithMostWater/A_BruteForce.java
@@ -0,0 +1,22 @@
+package com.backendml.array.B4_ContainerWithMostWater;
+
+public class A_BruteForce {
+    public int maxArea(int[] heights) {
+        int maxArea = 0;
+        int n = heights.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int containerHeight = Math.min(heights[i], heights[j]);
+                int containerWidth = j - i;
+
+                int currentArea = containerHeight * containerWidth;
+
+                if (currentArea > maxArea) {
+                    maxArea = currentArea;
+                }
+            }
+        }
+        return maxArea;
+    }
+}
