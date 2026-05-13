abebe41842fa22d043260f9b9d4aa94aa5827618
Nagesh Kumar <85521267+nkchauhan003@users.noreply.github.com>
2026-05-13 13:44:00 +0530

    Middle of the Linked List

---
+++ src/main/java/com/backendml/array/D1_MiddleOfTheLinkedList/D1_MiddleOfTheLinkedList.java
@@ -0,0 +1,32 @@
+package com.backendml.array.D1_MiddleOfTheLinkedList;
+
+public class D1_MiddleOfTheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+
+    }
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+
+
