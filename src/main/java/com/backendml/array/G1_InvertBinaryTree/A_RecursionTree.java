2c07f40e504c2532c493e0b7a320abbb66b904a1
Nagesh Kumar <85521267+nkchauhan003@users.noreply.github.com>
2026-05-15 17:09:37 +0530

    Invert Binary Tree

---
+++ src/main/java/com/backendml/array/G1_InvertBinaryTree/A_RecursionTree.java
@@ -0,0 +1,35 @@
+package com.backendml.array.G1_InvertBinaryTree;
+
+public class A_RecursionTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
