0ab527c99ff6f0b22ec3d769925df683e1560d61
Nagesh Kumar <85521267+nkchauhan003@users.noreply.github.com>
2026-05-15 11:36:22 +0530

    Binary Search

---
+++ src/main/java/com/backendml/array/F1_BinarySearch/A_Recursion.java
@@ -0,0 +1,22 @@
+package com.backendml.array.F1_BinarySearch;
+
+public class A_Recursion {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        return deltaSearch(nums, target, 0, nums.length - 1);
+    }
+
+    public int deltaSearch(int[] nums, int target, int start, int end) {
+        if (end < start) return -1; // Base case
+
+        int mid = start + (end - start) / 2; // Midpoint
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return deltaSearch(nums, target, start, mid - 1); // Search left half
+        } else {
+            return deltaSearch(nums, target, mid + 1, end);   // Search right half
+        }
+    }
+}
