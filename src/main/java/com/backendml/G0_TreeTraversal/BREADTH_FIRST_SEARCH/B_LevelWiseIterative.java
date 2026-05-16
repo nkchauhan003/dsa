13032d20b5cf4f9b0824a2f49d4961b7722080e7
Nagesh Kumar <85521267+nkchauhan003@users.noreply.github.com>
2026-05-16 16:59:37 +0530

    Lowest Common Ancestor of a Binary Search Tree

--- src/main/java/com/backendml/G0_TreeTraversal/BREADTH_FIRST_SEARCH/B_LevelWiseIterative.java
+++ src/main/java/com/backendml/G0_TreeTraversal/BREADTH_FIRST_SEARCH/B_LevelWiseIterative.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class B_Iterative {
+public class B_LevelWiseIterative {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> nodes = new ArrayList<>();
 
