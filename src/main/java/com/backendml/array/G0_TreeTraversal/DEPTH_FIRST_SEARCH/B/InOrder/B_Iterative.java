0471b99a21decf4827626c785a4e59b6cb46c5f0
Nagesh Kumar <85521267+nkchauhan003@users.noreply.github.com>
2026-05-16 12:35:32 +0530

    DFS Inorder Iterative

---
+++ src/main/java/com/backendml/array/G0_TreeTraversal/DEPTH_FIRST_SEARCH/B/InOrder/B_Iterative.java
@@ -0,0 +1,49 @@
+package com.backendml.array.G0_TreeTraversal.DEPTH_FIRST_SEARCH.B.InOrder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class B_Iterative {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> nodes = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode curr = root; //Avoid modifying the parameter pointer
+        while (curr != null || !stack.empty()) {
+            // Go as left as possible
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            // Process the current node
+            curr = stack.pop();
+            nodes.add(curr.val);
+
+            // Move to the right subtree
+            curr = curr.right;
+
+        }
+        return nodes;
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
