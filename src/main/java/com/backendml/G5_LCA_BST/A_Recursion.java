060fd745c7f0c48f10a6c53131bcc0e9128241b8
Nagesh Kumar <85521267+nkchauhan003@users.noreply.github.com>
2026-05-16 16:58:10 +0530

    Lowest Common Ancestor of a Binary Search Tree

--- src/main/java/com/backendml/G5_LCA_BST/A_Recursion.java
+++ src/main/java/com/backendml/G5_LCA_BST/A_Recursion.java
@@ -2,14 +2,21 @@ package com.backendml.G5_LCA_BST;
 
 public class A_Recursion {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // If tree is empty
+        if (root == null) {
+            return null;
+        }
+
+        // Both values are smaller -> look left
         if (p.val < root.val && q.val < root.val) {
             return lowestCommonAncestor(root.left, p, q);
         }
-
-        if (p.val > root.val && q.val > root.val) {
+        // Both values are larger -> look right
+        else if (p.val > root.val && q.val > root.val) {
             return lowestCommonAncestor(root.right, p, q);
         }
 
+        // Split point found or root matches p or q
         return root;
     }
 
