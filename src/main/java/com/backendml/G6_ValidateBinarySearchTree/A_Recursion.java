b44eaab3367e49eeb174f0fcccdcf71b541b1f12
Nagesh Kumar <85521267+nkchauhan003@users.noreply.github.com>
2026-05-16 17:27:51 +0530

    Validate Binary Search Tree

---
+++ src/main/java/com/backendml/G6_ValidateBinarySearchTree/A_Recursion.java
@@ -0,0 +1,35 @@
+package com.backendml.G6_ValidateBinarySearchTree;
+
+public class A_Recursion {
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValid(TreeNode root, long min, long max) {
+        if (root == null) return true;
+
+        if (root.val <= min || root.val >= max) return false;
+
+        return isValid(root.left, min, root.val) &&
+                isValid(root.right, root.val, max);
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
