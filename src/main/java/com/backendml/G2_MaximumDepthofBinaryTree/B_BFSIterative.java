c17686a606d9092cb1f47ff74dc20388aaf7cc66
Nagesh Kumar <85521267+nkchauhan003@users.noreply.github.com>
2026-05-16 15:00:14 +0530

    BFS Iterative

---
+++ src/main/java/com/backendml/G2_MaximumDepthofBinaryTree/B_BFSIterative.java
@@ -0,0 +1,53 @@
+package com.backendml.G2_MaximumDepthofBinaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class B_BFSIterative {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int depth = 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            depth++;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode currentNode = queue.poll();
+
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+        }
+        return depth;
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
