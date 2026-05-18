11f1fdf5d23e61617a69669e0aa4858913350270
Nagesh Kumar <85521267+nkchauhan003@users.noreply.github.com>
2026-05-18 11:34:12 +0530

    Clone Graph

--- src/main/java/com/backendml/K2_CloneGraph/B_BFSearch.java
+++ src/main/java/com/backendml/K2_CloneGraph/B_BFSearch.java
@@ -5,28 +5,38 @@ import java.util.*;
 public class B_BFSearch {
 
     public Node cloneGraph(Node node) {
+
         if (node == null)
             return null;
 
+        // BFS traversal queue
         Queue<Node> queue = new LinkedList<>();
         queue.offer(node);
 
+        // Clone starting node
         Node cloned = new Node(node.val);
 
+        // Original Node -> Cloned Node
         Map<Node, Node> map = new HashMap<>();
         map.put(node, cloned);
 
         while (!queue.isEmpty()) {
+
+            // Current original node
             Node existingNode = queue.poll();
+            // Corresponding cloned node
             Node clonedNode = map.get(existingNode);
 
+            // Traverse neighbors
             for (Node existingNeighbor : existingNode.neighbors) {
                 Node c = map.get(existingNeighbor);
+                // Clone neighbor if not cloned yet
                 if (c == null) {
                     queue.offer(existingNeighbor);
                     c = new Node(existingNeighbor.val);
                     map.put(existingNeighbor, c);
                 }
+                // Connect cloned neighbor
                 clonedNode.neighbors.add(c);
             }
         }
@@ -35,6 +45,7 @@ public class B_BFSearch {
     }
 
     private class Node {
+
         public int val;
         public List<Node> neighbors;
 
@@ -48,12 +59,10 @@ public class B_BFSearch {
             neighbors = new ArrayList<>();
         }
 
-        public Node(int _val, ArrayList<Node> _neighbors) {
+        public Node(int _val,
+                    ArrayList<Node> _neighbors) {
             val = _val;
             neighbors = _neighbors;
         }
     }
 }
-
-
-
